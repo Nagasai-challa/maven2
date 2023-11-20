bcf75fb49f40fd93e108be8396ebab02748e840b
Nagasai-challa <88236266+Nagasai-challa@users.noreply.github.com>
2023-11-20 19:03:23 +0530

    committing second maven

---
+++ src/main/java/test/mavendemo2/App.java
@@ -0,0 +1,13 @@
+package test.mavendemo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
